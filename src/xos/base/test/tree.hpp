cf9db2f06b797ba7f91461378cb7f50139ec41eb
medusade <medusade@cox.net>
2016-06-08 07:46:01 -0700

    Updated source.

--- src/xos/base/test/tree.hpp
+++ src/xos/base/test/tree.hpp
@@ -48,6 +48,34 @@ public:
 
     ///////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////
+    virtual int run(int argc, char_t** argv, char_t** env) {
+        int err = 0;
+        const char* a2z = "ABCDEFGHIJKLMNOPQRTSUVWXYZ";
+        xos::base::avl::branch b[26];
+        xos::base::avl::tree t;
+        size_t i, n;
+        for (i = (n = xos::base::chars_t::count(a2z)); i > 0; --i) {
+            xos::base::leaf& l = b[n-i].leaf();
+            xos::base::leaf::what_t& w = l.what();
+            w.assign(a2z + i - 1, 1);
+            t.insert(b[n-i]);
+        }
+        for (xos::base::avl::branch* b = t.first();  b; b = t.next(*b)) {
+            out(b->leaf().what().chars());
+        }
+        outln();
+        for (i = (n = xos::base::chars_t::count(a2z)); i > 0; --i) {
+            t.remove(b[n-i]);
+            for (xos::base::avl::branch* b = t.first();  b; b = t.next(*b)) {
+                out(b->leaf().what().chars());
+            }
+            outln();
+        }
+        return err;
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
 };
 
 } // namespace tree 
